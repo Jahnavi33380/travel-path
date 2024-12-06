c01ae620c85972e7f633d77ce45a8944913ff723
Jahnavi33380 <gonajahnavi3338@gmail.com>
2024-12-06 16:53:14 -0700

    solved merge conflicts.

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,12 @@ import Header from './components/custom/Header';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [userDetails, setUserDetails] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-
-  const handleLogin = (user) => {
-      setUserDetails(user);
-      setLoggedIn(true)
-    };
-
-        const [showEditPreferences, setShowEditPreferences] = useState(false);
-
-        const handleEditPreferences = () => {
-          setShowEditPreferences(true);
-        };
 
   return (
     <>
 
-     <Header
-            userDetails={userDetails}
-            onEditPreferences={handleEditPreferences}
-            setLoggedIn={setLoggedIn}
-          />
+      <Hero/>
 
-      <Hero userDetails={userDetails} onLogin={handleLogin} />
-       {showEditPreferences && (
-              <EditPreferences
-
-                userDetails={userDetails}
-              />
-            )}
     </>
   )
 }
