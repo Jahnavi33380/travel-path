31732b079ff7e72354fabb12c1bfe0ae33813d9f
Ashutosh Kumbhar <ashutoshkumbhar@Ashutoshs-MacBook-Pro.local>
2024-12-15 23:12:10 -0700

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import ghPages from 'vite-plugin-gh-pages';
+
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
