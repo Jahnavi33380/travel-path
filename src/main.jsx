5a04371b210e194bd6395aaa75a1df45b1b468f6
Ashutosh Kumbhar <ashutoshkumbhar@Ashutoshs-MacBook-Pro.local>
2024-12-16 00:35:11 -0700

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import CreateTrip from './create-trip'; // Adjust the path if necessary
 import CreateIternary from './iternary';
 import Header from './components/custom/Header';
@@ -13,33 +13,33 @@ import ManageInterests from './manage-interests';
 
 const router = createBrowserRouter([
   {
-    path: '/travel-path/',
-    element: <App />,
+    path:'/',
+    element:<App/>,
   },
   {
-    path: '/travel-path/create-trip',
-    element: <CreateTrip />,
+    path:'/create-trip',
+    element:<CreateTrip/>,
   },
   {
-    path: '/travel-path/view-trip/:tripId',
-    element: <ViewTrip />,
+    path:'/view-trip/:tripId',
+    element:<ViewTrip/>,
   },
   {
-    path: '/travel-path/iternary',
-    element: <CreateIternary />,
+    path:'/iternary',
+    element: <CreateIternary/>
   },
   {
-    path: '/travel-path/manage-interest',
-    element: <ManageInterests />,
-  },
-]);
+    path: '/manage-interest',
+    element: <ManageInterests></ManageInterests>
+  }
 
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
-      <Header />
-      <RouterProvider router={router} />
+    <Header/>
+    <RouterProvider router={router}  />
     </GoogleOAuthProvider>
   </React.StrictMode>,
 );
